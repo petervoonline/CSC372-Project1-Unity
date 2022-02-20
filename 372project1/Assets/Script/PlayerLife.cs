58b002799480156e0e06337ec98c1bd781a56c8b
jiwonseoUA <70163830+jiwonseoUA@users.noreply.github.com>
2022-02-19 20:03:03 -0700

    Update PlayerLife.cs

--- 372project1/Assets/Script/PlayerLife.cs
+++ 372project1/Assets/Script/PlayerLife.cs
@@ -3,17 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-
+/*
+This class is to implement the life cycle of player. Living -> Death(game over), or Death -> Life(ReStart)
+*/
 public class PlayerLife : MonoBehaviour
 {   
-    private static int death = 0;
+    //Private fields
+    private static int death = 0; //how many times player have died. 
+                                // It is static field because it implemented until (death < 5)
     private Rigidbody2D rb;
     private Animator anim;
-
+    //These variables can be modified in Unity IDE
     [SerializeField] private Text lifeText;
     [SerializeField] private Text go_text;
 
-    // Start is called before the first frame update
+    /*
+    Start is called before the first frame update
+    And it shows the initial remaining life in the text. This needs inside of start() because it needs to be updated 
+    everytime player die or restart the game. 
+    */
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -22,7 +30,7 @@ public class PlayerLife : MonoBehaviour
         
     }
 
-    // Update the game to Game Over scene if the player used up 5 attempts
+    /* Update the game to Game Over scene if the player used up 5 attempts*/
     void Update() {
         if (getLife() == 0) {
             Time.timeScale = 0;
@@ -31,33 +39,36 @@ public class PlayerLife : MonoBehaviour
         }
         
     }
-
+    /*
+    This method is recognizing player on trap and increment the number of death
+    If player eats cherry 
+    */
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        
         if (collision.gameObject.CompareTag("Trap"))
         {
             death++;
             Die();
-
-        } else if (collision.gameObject.CompareTag("Cherry"))
-        {
-            addLife();
-            lifeText.text = "Lives: " + getLife();
         }
     }
+    /*
+    This method is implemented when player hit trap (obstacle)
+    */
     private void Die()
     {
         rb.bodyType = RigidbodyType2D.Static; 
         anim.SetTrigger("death");
     }
-
+    
+    /*
+    This game is implemented to restart the game with new scene
+    */
     private void RestartLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
-    // calculate the attempt the player has left
+    /* calculate the attempt the player has left*/
     private int getLife() {
         return (5-death);
     }
