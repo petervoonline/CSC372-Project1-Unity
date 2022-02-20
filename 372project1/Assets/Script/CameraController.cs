812d1cccf66be3b373ae99dbc7b67ebb6c336098
jiwonseoUA <70163830+jiwonseoUA@users.noreply.github.com>
2022-02-19 19:38:29 -0700

    Update CameraController.cs

--- 372project1/Assets/Script/CameraController.cs
+++ 372project1/Assets/Script/CameraController.cs
@@ -1,7 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/*
+This class is controlling the camera movement by updating the camera location by its x position, y position, and z position.
+[serializeField] means you can update the player position on the Unity IDE not only from C# script.
+*/
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private Transform player;
